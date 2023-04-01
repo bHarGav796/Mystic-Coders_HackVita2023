8e973b087226c5222f1221224d94ea9b98174cb1
Gyan <90141014+GyanBikashMili@users.noreply.github.com>
2023-04-01 09:26:54 +0530

    Updated MainActivity Class

--- app/src/main/java/com/example/attendancemark/MainActivity.kt
+++ app/src/main/java/com/example/attendancemark/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.attendancemark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.LayoutInflater
+import com.example.loginpage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        binding.btnRegister.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
